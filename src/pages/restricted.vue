5c812fe99ba919e2d8af894d7763fd5b7afb3ccd
Frzz2z <138352677+Frzz2z@users.noreply.github.com>
2023-11-26 16:55:20 +0700

    Update restricted.vue

--- src/pages/restricted.vue
+++ src/pages/restricted.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import { useAuthStore} from '@/stores/auth';
+
+const auth = useAuthStore();
+</script>
+
 <template>
-    <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt quo sapiente quidem eos vero delectus dolorum dolorem distinctio, officia animi, nihil odit inventore ullam repudiandae enim! Quidem, voluptatibus libero?
+    <div v-if="auth.username `!=` ''">
+        Lorem ipsum dolor sit amet consectetur adipisicing 
+        elit. Magnam deserunt quo sapiente quidem eos vero 
+        delectus dolorum dolorem distinctio, officia animi, 
+        nihil odit inventore ullam repudiandae enim! Quidem,
+         voluptatibus libero?
+    </div>
+    <div v-else>
+        Please login first to see the content
     </div>
 </template>
