df212daead25649cf813159ea80dbc9f72b906fd
Frzz2z <138352677+Frzz2z@users.noreply.github.com>
2023-11-26 21:29:05 +0700

    Update login.vue

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,54 +1,62 @@
 <template>
-    <div>
-      <h1>Login Page</h1>
-      <div class="flex flex-col gap-2">
+  <div class="flex items-center justify-center h-96">
+    <div class="bg-white p-8 rounded shadow-inner w-96">
+      <h1 class="text-2xl text-center font-semibold mb-4">Login Page</h1>
+      <div class="flex flex-col gap-4">
         <input
           type="text"
           required
           v-model="username"
-          class="border"
+          class="border p-2 rounded"
           placeholder="Username"
         />
         <input
           type="password"
           required
           v-model="password"
-          class="border"
+          class="border p-2 rounded"
           placeholder="Password"
         />
         <button
-          @click="onLogin()"
-          :disabled="!isFormValid"
-          class="bg-blue-500 text-white py-1 px-3"
+          @click="onLogin"
+          class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
         >
           Login
         </button>
       </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from "vue";
-  import { useRouter } from "vue-router";
-  import { useAuthStore } from "@/stores/auth";
-  
-  const auth = useAuthStore();
-  const username = ref("");
-  const password = ref("");
-  const router = useRouter();
-  
-  // Check if both username and password are not empty
-  const isFormValid = () => {
-    return username.value.trim() !== "" && password.value.trim() !== "";
-  };
-  
-  const onLogin = () => {
-    if (isFormValid()) {
-      auth.login(username.value); // You may adjust this part according to your authentication logic
-      router.push("/");
-    } else {
-      // Handle invalid login attempt, e.g., show an error message
-      alert("Please enter username and password");
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+import axios from "axios";
+
+const auth = useAuthStore();
+const username = ref("");
+const password = ref("");
+const router = useRouter();
+
+const onLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/login', {
+        username: username.value,
+        password: password.value,
+      });
+
+      console.log(response)
+
+      if (response.data.status === 'success') {
+        auth.login(username.value);
+        router.push('/');
+      } else {
+        window.alert('Username atau password salah');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      window.alert('Terjadi kesalahan saat melakukan login');
     }
   };
-  </script>
+</script>
